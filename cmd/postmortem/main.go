d12dd998910ebbac7ee7f826879166de4c130da7
Ryan Kidd <itsryankidd@gmail.com>
2026-08-11 09:22:18 -0700

    Wire git source into the CLI with a repeatable --git-repo flag

--- cmd/postmortem/main.go
+++ cmd/postmortem/main.go
@@ -6,11 +6,26 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/ryankidd/postmortem/git"
 	"github.com/ryankidd/postmortem/journald"
 	"github.com/ryankidd/postmortem/timeline"
 )
+
+// repoPaths collects a repeatable --git-repo flag.
+type repoPaths []string
+
+func (p *repoPaths) String() string { return strings.Join(*p, ",") }
+
+func (p *repoPaths) Set(v string) error {
+	if v == "" {
+		return fmt.Errorf("repository path required")
+	}
+	*p = append(*p, v)
+	return nil
+}
 
 func main() {
 	if err := run(os.Args[1:], os.Stdout); err != nil {
@@ -24,6 +39,8 @@ func run(args []string, out *os.File) error {
 	since := fs.String("since", "1h", "start of the window (RFC3339 timestamp or duration back from now)")
 	until := fs.String("until", "", "end of the window (RFC3339 timestamp, default now)")
 	unit := fs.String("unit", "", "restrict journald events to this systemd unit (default: all units)")
+	var repos repoPaths
+	fs.Var(&repos, "git-repo", "include commits and tags from this git repository (repeatable)")
 	if err := fs.Parse(args); err != nil {
 		return err
 	}
@@ -35,6 +52,9 @@ func run(args []string, out *os.File) error {
 
 	sources := []timeline.Source{
 		journald.Source{Unit: *unit},
+	}
+	for _, repo := range repos {
+		sources = append(sources, git.Source{Repo: repo})
 	}
 
 	events, err := timeline.Build(sources, sinceTime, untilTime)
